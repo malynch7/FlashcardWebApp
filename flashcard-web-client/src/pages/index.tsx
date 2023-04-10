13b5c9e100f8f0dfb86306994be41fcf305c6713
Mark Lynch <mark.lynch@improving.com>
2023-04-10 11:36:22 -0400

    cleaned index

--- flashcard-web-client/src/pages/index.tsx
+++ flashcard-web-client/src/pages/index.tsx
@@ -5,12 +5,20 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
     return (
-        <main className={styles.main}>
-                <h1>Welcome to Flashcard App!</h1>
-                <Link href="/play"><Button variant="contained">Play</Button></Link>
-                <Link href="/create"><Button variant="contained">Create</Button></Link>
-                <Link href="/practice"><Button variant="contained">Practice</Button></Link>
-        </main>
+        <div className={styles.main}>
+            <h1>Welcome to Flashcard App!</h1>
+            <div className={styles.button_group}>
+                <Link href="/play">
+                    <Button variant="contained">Play</Button>
+                </Link>
+                <Link href="/create">
+                    <Button variant="contained">Create</Button>
+                </Link>
+                <Link href="/practice">
+                    <Button variant="contained">Practice</Button>
+                </Link>
+            </div>
+        </div>
     );
 };
 

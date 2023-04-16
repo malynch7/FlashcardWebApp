ac7a5875d102b0cf476b7ba08e898f18d61a3835
Mark Lynch <mark.lynch@improving.com>
2023-04-16 08:29:48 -0400

    mobile navbar

--- flashcard-web-client/src/components/Navbar/Navbar.tsx
+++ flashcard-web-client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,12 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import {
+    AppBar,
+    Box,
+    Button,
+    IconButton,
+    Toolbar,
+    Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import styles from "./Navbar.module.css";
 import { FC } from "react";
 import Link from "next/link";
@@ -7,10 +15,19 @@ export const Navbar: FC = () => {
     return (
         <AppBar className={styles.appbar}>
             <Toolbar className={styles.toolbar}>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    className={styles.menu_btn}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Link href="/" className={styles.logo}>
+                    <Typography variant="h4">Flashcard App</Typography>
+                </Link>
                 <Box className={styles.primary_button_group}>
-                    <Link href="/">
-                        <Typography variant="h4">Flashcard App</Typography>
-                    </Link>
                     <Link href="/play">
                         <Button color="inherit">Play</Button>
                     </Link>
@@ -18,7 +35,9 @@ export const Navbar: FC = () => {
                         <Button color="inherit">Practice</Button>
                     </Link>
                 </Box>
-                <Button color="inherit">Login</Button>
+                <Box className={styles.secondary_button_group}>
+                    <Button color="inherit">Login</Button>
+                </Box>
             </Toolbar>
         </AppBar>
     );

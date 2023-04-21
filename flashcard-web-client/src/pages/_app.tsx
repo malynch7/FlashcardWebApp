4794df917ad727fe7ce2e855f13d85e5d135cd3a
Mark Lynch <malynch7@gmail.com>
2023-04-21 09:55:05 -0400

    b2c auth

--- flashcard-web-client/src/pages/_app.tsx
+++ flashcard-web-client/src/pages/_app.tsx
@@ -15,11 +15,24 @@ import Head from "next/head";
 // MSAL configuration
 const configuration: Configuration = {
     auth: {
-        clientId: "Tal8Q~KOkYKm2F1S9gDoH62trh2oGKDMMjEZxa.l",
+        clientId: "18115a2a-4e24-46c1-8cab-f18ed23f3ba2",
+        authority:
+            "https://mlynchdev.b2clogin.com/mlynchdev.onmicrosoft.com/B2C_1_register",
+        knownAuthorities: ["mlynchdev.b2clogin.com"],
+        redirectUri: "http://localhost:3000/",
     },
 };
 
 const msalInstance = new PublicClientApplication(configuration);
+
+msalInstance
+    .handleRedirectPromise()
+    .then((tokenResponse) => {
+        console.log(tokenResponse);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (

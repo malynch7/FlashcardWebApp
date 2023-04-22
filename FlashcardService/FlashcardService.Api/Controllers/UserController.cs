3a7e7c89ba4ab30f0c3218ab3159beb52544f89b
Mark Lynch <malynch7@gmail.com>
2023-04-21 21:45:10 -0400

    + b2c auth

--- FlashcardService/FlashcardService.Api/Controllers/UserController.cs
+++ FlashcardService/FlashcardService.Api/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 using FlashcardService.Domain.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlashcardService.Api.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
--- FlashcardService/FlashcardService.Api/Program.cs
+++ FlashcardService/FlashcardService.Api/Program.cs
@@ -3,7 +3,9 @@ using FlashcardService.Domain.Repositories;
 using FlashcardService.Domain.Services;
 using FlashcardService.Infrastructure.Data;
 using FlashcardService.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,6 +32,16 @@ builder.Services.AddCors(options =>
                       });
 });
 
+// AZURE B2C
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options);
+
+                options.TokenValidationParameters.NameClaimType = "name";
+            },
+    options => { builder.Configuration.Bind("AzureAdB2C", options); });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -46,6 +58,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
